182edb57eca8d6bda403c145a0feadad4f0c5969
Ruth Newman <ruthnewman@gmail.com>
2020-02-04 00:13:07 +0000

    hack redirect works

---
+++ xss/hacker.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hacked Off!</title>
+    </head>
+    <body>
+        <h1>You have been hacked!</h1>
+    </body>
+</html>
