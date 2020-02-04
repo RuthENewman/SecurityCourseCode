4f51a8997ad1efaba897c6c5d85d8b29fb8b1cfb
Ruth Newman <ruthnewman@gmail.com>
2020-02-04 00:42:24 +0000

    password hashing

---
+++ password/index.php
@@ -0,0 +1,51 @@
+<?php 
+
+$user = 'root';
+$password = 'root';
+$db = 'sql_injection';
+$host = 'localhost';
+$port = 8889;
+
+$con = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+
+if (isset($_POST['submit'])) {
+    $email = trim($_POST['email']);
+    
+    $userQuery = $con->prepare("SELECT * FROM users WHERE email = :email");
+
+    $userQuery->execute([
+        'email' => $email
+    ]);
+
+    if ($userQuery->rowCount()) {
+        echo "WE FOUND A USER";
+    }
+}
+
+?>
+
+<!doctype html>
+<html lang=en>
+    <head>
+        <meta charset="utf-8">
+        <title>Password hashing</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="col-sm-6 col-sm-offset-3">
+            <form action="" method="post" autocomplete="off">
+                <h2>Login</h2>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="text" name="password" class="form-control"/>
+                </div>
+                <input type="submit" name="submit" class="btn btn-primary">
+            </form>
+        </div>
+    </body>
+
+</html>
--- sql-injection/index.php
+++ sql-injection/index.php
@@ -31,15 +31,15 @@ if (isset($_POST['email'])) {
 
 
 
-$link = mysqli_init();
-$success = mysqli_real_connect(
-   $link,
-   $host,
-   $user,
-   $password,
-   $db,
-   $port
-);
+// $link = mysqli_init();
+// $success = mysqli_real_connect(
+//    $link,
+//    $host,
+//    $user,
+//    $password,
+//    $db,
+//    $port
+// );
 
 ?>
 
