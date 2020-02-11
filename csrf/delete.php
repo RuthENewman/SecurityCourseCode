35355346a187d0ef8b5ad84d558ef5e1da660414
Ruth Newman <ruthnewman@gmail.com>
2020-02-11 23:38:43 +0000

    cleaner code

--- csrf/delete.php
+++ csrf/delete.php
@@ -2,28 +2,13 @@
 
 require_once 'app/config.php';
 
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $delete_user = $con->prepare("DELETE FROM users WHERE name = :name");
 
-
-if($_SERVER['REQUEST_METHOD'] !== 'POST'){
-
-
-    die('SORRY CANNOT PERFORM THIS OPERATION');
-
-
+    $delete_user->execute([
+        'name' => $_SESSION['name']
+    ]);
+    
 }
-
-
-
-$delete_user = $con->prepare("DELETE FROM users WHERE name = :name");
-
-
-$delete_user->execute([
-
-    'name' => $_SESSION['name']
-
-
-]);
-
-
 
 ?>
